d2644237571f9fb4d74139245eba1262ca34b842
Ron Evans <ron@hybridgroup.com>
2018-02-11 20:18:16 +0100

    Caffe GoogleNet classifier fully functional
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- cmd/caffe/main.go
+++ cmd/caffe/main.go
@@ -1,6 +1,15 @@
+// How to use:
+//
+// You must have OpenCV/GoCV installed to use this example.
+// Run the following commands:
+//
+// 	source $GOPATH/src/gocv.io/x/gocv/env.sh
+// 	go run ./cmd/caffe/main.go 0 ~/Development/ncsdk/examples/caffe/GoogLeNet/graph ~/Development/ncsdk/examples/data/images/cat.jpg ~/Development/ncsdk/examples/data/ilsvrc12/synset_words.txt
+//
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"image"
 	"io/ioutil"
@@ -15,6 +24,7 @@ func main() {
 	deviceID, _ := strconv.Atoi(os.Args[1])
 	graphFileName := os.Args[2]
 	imageFileName := os.Args[3]
+	descriptions, _ := readDescriptions(os.Args[4])
 
 	res, name := ncs.GetDeviceName(deviceID)
 	if res != ncs.StatusOK {
@@ -51,12 +61,17 @@ func main() {
 	// load image file
 	img := gocv.IMRead(imageFileName, gocv.IMReadColor)
 
-	// convert to format needed
-	blob := gocv.BlobFromImage(img, 1.0, image.Pt(224, 224), gocv.NewScalar(104, 117, 123, 0), false, false)
+	// convert to format needed by NCS
+	resized := gocv.NewMat()
+	gocv.Resize(img, resized, image.Pt(224, 224), 0, 0, gocv.InterpolationDefault)
+
+	fp32Image := gocv.NewMat()
+	resized.ConvertTo(fp32Image, gocv.MatTypeCV32F)
+
+	fp16Blob := fp32Image.ConvertFp16()
 
 	// load tensor into graph
 	fmt.Println("Loading tensor...")
-	fp16Blob := blob.ConvertFp16()
 	loadStatus := graph.LoadTensor(fp16Blob.ToBytes())
 	if loadStatus != ncs.StatusOK {
 		fmt.Println("Error loading tensor data:", loadStatus)
@@ -70,11 +85,34 @@ func main() {
 		return
 	}
 
-	fmt.Println(data)
+	// convert results from fp16 back to float32
+	fp16Results := gocv.NewMatFromBytes(1, len(data)/2, gocv.MatTypeCV16S, data)
+	results := fp16Results.ConvertFp16()
+
+	// determine the most probable classification
+	_, maxVal, _, maxLoc := gocv.MinMaxLoc(results)
+	fmt.Printf("description: %v %v, maxVal: %v\n", maxLoc, descriptions[maxLoc.X], maxVal)
 
 	// deallocate graph
 	fmt.Println("Deallocating graph...")
 	graph.DeallocateGraph()
 
 	fmt.Println("Done.")
+}
+
+// readDescriptions reads the descriptions from a file
+// and returns a slice of its lines.
+func readDescriptions(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
