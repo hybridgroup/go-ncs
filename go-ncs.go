c3a6f1348db80dc8aa7d9319c15d823abaa1d09d
Ron Evans <ron@hybridgroup.com>
2018-02-12 10:26:19 +0100

    Complete GoDoc comments for all currently implemented functions
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- go-ncs.go
+++ go-ncs.go
@@ -1,3 +1,8 @@
-// Package ncs is a Go language wrapper around the Movidius Neural Compute Stick SDK
+// Package ncs is a Go language wrapper for the Intel® Movidius™ Neural Compute Stick (NCS)
+//
+// For more information go to:
+// https://developer.movidius.com/
+//
+// You must have the Intel Movidius NCS hardware in order to use this package.
 //
 package ncs
--- ncs.go
+++ ncs.go
@@ -58,14 +58,22 @@ type Graph struct {
 	GraphHandle unsafe.Pointer
 }
 
-// GetDeviceName gets the name of the NCS stick at index.
+// GetDeviceName gets the name of the NCS stick located at index.
+//
+// For more information:
+// https://movidius.github.io/ncsdk/c_api/mvncGetDeviceName.html
+//
 func GetDeviceName(index int) (Status, string) {
 	buf := make([]byte, 100)
 	ret := Status(C.ncs_GetDeviceName(C.int(index), (*C.char)(unsafe.Pointer(&buf[0]))))
 	return ret, string(buf)
 }
 
-// OpenDevice
+// OpenDevice initializes an NCS device and returns a Stick.
+//
+// For more information:
+// https://movidius.github.io/ncsdk/c_api/mvncOpenDevice.html
+//
 func OpenDevice(name string) (Status, *Stick) {
 	cName := C.CString(name)
 	defer C.free(unsafe.Pointer(cName))
@@ -75,31 +83,53 @@ func OpenDevice(name string) (Status, *Stick) {
 	return Status(ret), &Stick{DeviceHandle: deviceHandle}
 }
 
-// CloseDevice
+// CloseDevice closes a previously opened NCS device.
+//
+// For more information:
+// https://movidius.github.io/ncsdk/c_api/mvncCloseDevice.html
+//
 func (s *Stick) CloseDevice() Status {
 	res := C.ncs_CloseDevice(s.DeviceHandle)
 	s.DeviceHandle = nil
 	return Status(res)
 }
 
-// AllocateGraph
+// AllocateGraph allocates a graph for use on an NCS device, and
+// returns a Graph.
+//
+// For more information:
+// https://movidius.github.io/ncsdk/c_api/mvncAllocateGraph.html
+//
 func (s *Stick) AllocateGraph(graphData []byte) (Status, *Graph) {
 	var graphHandle unsafe.Pointer
 	ret := Status(C.ncs_AllocateGraph(s.DeviceHandle, &graphHandle, unsafe.Pointer(&graphData[0]), C.uint(len(graphData))))
 	return ret, &Graph{GraphHandle: graphHandle}
 }
 
-// DeallocateGraph
+// DeallocateGraph deallocates and frees resources for a Graph.
+//
+// For more information:
+// https://movidius.github.io/ncsdk/c_api/mvncDeallocateGraph.html
+//
 func (g *Graph) DeallocateGraph() Status {
 	return Status(C.ncs_DeallocateGraph(g.GraphHandle))
 }
 
-// LoadTensor
+// LoadTensor starts inference on the NCS by providing input to the neural network.
+//
+// For more information:
+// https://movidius.github.io/ncsdk/c_api/mvncLoadTensor.html
+//
 func (g *Graph) LoadTensor(tensorData []byte) Status {
 	return Status(C.ncs_LoadTensor(g.GraphHandle, unsafe.Pointer(&tensorData[0]), C.uint(len(tensorData))))
 }
 
-// GetResult
+// GetResult retrieves the result of an inference that was previously initiated
+// using the LoadTensor() method.
+//
+// For more information:
+// https://movidius.github.io/ncsdk/c_api/mvncGetResult.html
+//
 func (g *Graph) GetResult() (Status, []byte) {
 	resultData := C.struct_ResultData{}
 	status := C.ncs_GetResult(g.GraphHandle, &resultData)
